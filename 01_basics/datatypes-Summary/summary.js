3350c79802c56639059d744f95b8f8a54135d8e6
Namit <144468004+menamiit@users.noreply.github.com>
2024-01-10 09:18:36 +0000

    summary and changes

---
+++ 01_basics/datatypes-Summary/summary.js
@@ -0,0 +1,30 @@
+// two types of data types based on the basis of storage of data in memory and how the data can be accessed 
+
+// 1. Primitive 
+// 2. Non-Primitive or Reference Type
+
+/* Primitive
+
+7 types: String, Number, Boolean, null, undefined, Symbol, BigInt.*/
+
+/* Reference (Non-Primitive):
+
+Array, Objects, Functions
+
+
+/* Return type of variables in JavaScript
+
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+       
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object */
