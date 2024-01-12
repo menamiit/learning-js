700525b7586453f5bff08e1091e2efbe8e5be16d
Namit <pareeknamit8@gmail.com>
2024-01-12 14:35:55 +0000

    dates

---
+++ 01_basics/09_dates.js
@@ -0,0 +1,9 @@
+const newDate = new Date()
+
+console.log(newDate.toDateString());
+console.log(newDate.toISOString());
+console.log(newDate.toJSON());
+console.log(newDate.toLocaleDateString());
+console.log(newDate.toLocaleTimeString('en-It'));
+console.log(newDate.toString());
+console.log(newDate.getTimezoneOffset());
