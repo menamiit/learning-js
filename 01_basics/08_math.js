c8e34f452f61321cf41ccea3fd3f843715ea66a3
Namit <pareeknamit8@gmail.com>
2024-01-12 18:28:32 +0000

    modification

--- 01_basics/08_math.js
+++ 01_basics/08_math.js
@@ -15,3 +15,19 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+// ---NUMBER METHODS---
+
+
+// Number.parseFloat() =	Parses a string argument and returns a floating point number. Same as the global parseFloat() function.
+
+// Number.parseInt()	=   Parses a string argument and returns an integer of the specified radix or base. Same as the global parseInt() function.
+
+// Number.isFinite()	=   Determines whether the passed value is a finite number.
+
+// Number.isInteger()	=   Determines whether the passed value is an integer.
+
+// Number.isNaN()	    =   Determines whether the passed value is NaN. More robust version of the original global isNaN().
+
+// Number.isSafeInteger()=	Determines whether the provided value is a number that is a safe integer.
