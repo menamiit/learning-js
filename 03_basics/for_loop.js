1f5ef39cb9378e083ad1ce0a743244b7c8d66fc0
Namit <pareeknamit8@gmail.com>
2025-06-06 21:03:31 +0530

    added 03_basics with for and while loop

---
+++ 03_basics/for_loop.js
@@ -0,0 +1,7 @@
+for(let i=0; i<5; i++) {
+    // let j=5;
+    for(let j=0; j<=i; j++) {
+        process.stdout.write("*");
+    }
+    console.log("\n");
+}
---
+++ 03_basics/while_loop.js
@@ -0,0 +1,7 @@
+let n = 5;
+let result = n;
+while(n>1){
+    result *= n-1;
+    n--;
+}
+console.log(result);
