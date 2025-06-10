fa68a48c70713eccbc4367968663be4e469a275b
Namit <pareeknamit8@gmail.com>
2025-06-10 12:20:17 +0530

    Delete Basics directory

--- Basics/03_basics/for_loop.js
+++
@@ -1,7 +0,0 @@
-for(let i=0; i<5; i++) {
-    // let j=5;
-    for(let j=0; j<=i; j++) {
-        process.stdout.write("*");
-    }
-    console.log("\n");
-}
--- Basics/03_basics/while_loop.js
+++
@@ -1,7 +0,0 @@
-let n = 5;
-let result = n;
-while(n>1){
-    result *= n-1;
-    n--;
-}
-console.log(result);
